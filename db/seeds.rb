42ddb6d08325f3ff6400844ddd3eba53f2ce60df
jspencerhopkins <jspencerhopkins@gmail.com>
2015-03-14 16:33:59 -0400

    Seed file with basic job data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,26 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# 1st try - only going to use the most recent 50 job postings
+angel_jobs = AngellistApi.get_jobs(options={}).jobs
+
+
+
+angel_jobs.each do |job|
+
+  Job.create( al_url:         job.angellist_url,
+              al_created_at:  job.created_at,
+              currency_code:  job.currency_code,
+              description:    job.description,
+              equity_cliff:   job.equity_cliff,
+              equity_max:     job.equity_max,
+              equity_vest:    job.equity_vest,
+              al_job_id:      job.id,
+              job_type:       job.job_type,
+              remote_ok:      job.remote_ok,
+              salary_max:     job.salary_max,
+              salary_min:     job.salary_min,
+              al_start_id:    job.startup.id,
+              title:          job.title,
+              al_updated_at:  job.updated_at
+              )
+
+end
