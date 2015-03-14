487eb5d17e0abf5e9b4affbcecd1ab4213a319b5
jspencerhopkins <jspencerhopkins@gmail.com>
2015-03-14 14:34:27 -0400

    Set up Job model

---
+++ db/migrate/20150314183307_create_jobs.rb
@@ -0,0 +1,26 @@
+class CreateJobs < ActiveRecord::Migration
+  def change
+    create_table :jobs do |t|
+      t.string :al_job_url
+      t.datetime :al_created_at
+      t.string :currency_code
+      t.string :description
+      t.string :equity_cliff
+      t.string :equity_max
+      t.string :equity_min
+      t.string :equity_vest
+      t.integer :al_job_id
+      t.string :job_type
+      t.boolean :remote_ok
+      t.integer :salary_max
+      t.integer :salary_min
+      t.string :skilltag_display_names
+      t.integer :al_start_id
+      t.integer :al_loc_id
+      t.string :title
+      t.datetime :updated_at
+
+      t.timestamps null: false
+    end
+  end
+end
