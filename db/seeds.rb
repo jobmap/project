ffbc100c20bf063866fb3fe65655baddd263ff55
jspencerhopkins <jspencerhopkins@gmail.com>
2015-03-15 16:00:39 -0400

    Setup seed for job, startup, location

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,15 @@ angel_jobs = AngellistApi.get_jobs(options={}).jobs
 
 angel_jobs.each do |job|
 
+  # Number of tags in tag object
+  num_tags = job.tags.length
+  loc_tag = num_tags - 2
+  role_tag = num_tags - 1
+
   Job.create( al_url:         job.angellist_url,
               al_created_at:  job.created_at,
               currency_code:  job.currency_code,
+              role_name:      job.tags[role_tag].display_name
               description:    job.description,
               equity_cliff:   job.equity_cliff,
               equity_max:     job.equity_max,
@@ -19,8 +25,34 @@ angel_jobs.each do |job|
               salary_max:     job.salary_max,
               salary_min:     job.salary_min,
               al_start_id:    job.startup.id,
+              al_loc_id:      job.tags[loc_tag].id,
               title:          job.title,
               al_updated_at:  job.updated_at
               )
+
+  Startup.find_by_al_start_id(job.startup.id) ||
+    Startup.create( al_url:             job.startup.angellist_url,
+                    community_profile:  job.startup.community_profile,
+                    url:                job.startup.company_url,
+                    al_created_at:      job.startup.created_at,
+                    follower_count:     job.startup.follower_count,
+                    hidden:             job.startup.hidden,
+                    high_concept:       job.startup.high_concept,
+                    al_start_id:        job.startup.id,
+                    logo_url:           job.startup.logo_url,
+                    name:               job.startup.name,
+                    prod_desc:          job.startup.product_desc,
+                    quality:            job.startup.quality,
+                    thumb_url:          job.startup.thumb_url,
+                    al_updated_at:      job.startup.updated_at
+                    )
+
+  Location.find_by_al_loc_id(job.tags[loc_tag].id) ||
+    Location.create(  al_url:       job.tags[loc_tag].angellist_url,
+                      display_name: job.tags[loc_tag].display_name,
+                      al_loc_id:    job.tags[loc_tag].id,
+                      name:         job.tags[loc_tag].name
+                    )
+
 
 end
