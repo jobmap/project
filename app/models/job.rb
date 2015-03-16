7a4deb886b1bb5c6fc7256e9b43c62d0294caa9e
jspencerhopkins <jspencerhopkins@gmail.com>
2015-03-16 13:59:46 -0400

    Reset primary keys & set associations b/w Job & Location models

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,2 +1,7 @@
 class Job < ActiveRecord::Base
+
+  self.primary_key = 'al_job_id'
+
+  belongs_to :location, foreign_key: 'al_loc_id'
+  
 end
--- app/models/location.rb
+++ app/models/location.rb
@@ -1,2 +1,7 @@
 class Location < ActiveRecord::Base
+
+  self.primary_key = 'al_loc_id'
+  
+  has_many :jobs, foreign_key: 'al_loc_id'
+
 end
