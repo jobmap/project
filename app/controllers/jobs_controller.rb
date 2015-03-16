71b0713cc9faee0accd4b8a46e21cacc321d2603
Peterortiz82 <peterortiz82@gmail.com>
2015-03-16 19:26:32 -0400

    remove address, latitude, and longitude columns from jobs table

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,32 +1,14 @@
 class JobsController < ApplicationController
-  before_action :set_job, only: [:edit, :update, :destroy]
 
   def show
-      @jobs = Job.all
-      @hash = Gmaps4rails.build_markers(@jobs) do |job, marker|
-      marker.lat job.latitude
-      marker.lng job.longitude
-      marker.infowindow job.title
+      @location = Location.last
+      @hash = Gmaps4rails.build_markers(@location) do |location, marker|
+      marker.lat location.latitude
+      marker.lng location.longitude
     end
-  end
-
-  def new
-    @job = Job.new
-  end
- 
-  def edit
-  end
-
-  def create
-    @job = Job.create(job_params)
-    redirect_to root_url
   end
 
   private
-
-    def set_job
-      @job = Job.find(params[:id])
-    end
 
     def job_params
       params.require(:job).permit(:latitude, :longitude, :address)
--- db/migrate/20150316004521_add_location_columns_to_job.rb
+++
@@ -1,7 +0,0 @@
-class AddLocationColumnsToJob < ActiveRecord::Migration
-  def change
-    add_column :jobs, :latitude, :float
-    add_column :jobs, :longitude, :float
-    add_column :jobs, :address, :string
-  end
-end
