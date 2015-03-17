2c9db5d7a95fcb28c0cbbe185d63c30cc9b3744f
Peterortiz82 <peterortiz82@gmail.com>
2015-03-17 13:42:37 -0400

    create search function to search by location adrress

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,11 +1,17 @@
 class JobsController < ApplicationController
 
   def show
-      @location = Location.last
-      @hash = Gmaps4rails.build_markers(@location) do |location, marker|
+    @jobs = Job.all
+    @location = Location.where(address: params[:search])
+    @hash = Gmaps4rails.build_markers(@location) do |location, marker|
       marker.lat location.latitude
       marker.lng location.longitude
     end
-  end
 
+    if @location.empty?
+      redirect_to "/"
+    elsif @location.nil?
+      redirect_to "/"
+    end
+  end
 end
