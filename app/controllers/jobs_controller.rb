c14a30fc4eb2a737e312e2501cfea8a84a70acd8
Peterortiz82 <peterortiz82@gmail.com>
2015-03-18 09:50:48 -0400

    Remove @jobs variable from jobs controller

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,17 +1,16 @@
 class JobsController < ApplicationController
 
-  def show
-    @locations = Location.near(params[:search], 25)
-    @hash = Gmaps4rails.build_markers(@locations) do |locations, marker|
-      marker.lat locations.latitude
-      marker.lng locations.longitude
-    end
+ def show
+  @locations = Location.near(params[:search], 25)
+   @hash = Gmaps4rails.build_markers(@locations) do |locations, marker|
+     marker.lat locations.latitude
+     marker.lng locations.longitude
+   end
 
-    if @locations.empty?
-      redirect_to "/"
-    elsif @locations.nil?
-      redirect_to "/"
-    end
-  end
+   if @locations.empty?
+     redirect_to "/"
+   elsif @locations.nil?
+     redirect_to "/"
+   end
+ end
 end
-
