38c40dca90257b0af60c36290eb8c9bd9b8b92e6
jspencerhopkins <jspencerhopkins@gmail.com>
2015-03-18 21:06:47 -0400

    Add city & state columns to locations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150318192952) do
+ActiveRecord::Schema.define(version: 20150319010359) do
 
   create_table "jobs", force: :cascade do |t|
     t.string   "al_url"
@@ -48,6 +48,8 @@ ActiveRecord::Schema.define(version: 20150318192952) do
     t.float    "latitude"
     t.float    "longitude"
     t.string   "zipcode"
+    t.string   "city"
+    t.string   "state"
   end
 
   create_table "startups", force: :cascade do |t|
