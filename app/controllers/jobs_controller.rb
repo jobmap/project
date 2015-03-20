1f4e3acbe136df530eb65246c15d9aafd0755892
Peterortiz82 <peterortiz82@gmail.com>
2015-03-19 23:27:27 -0400

    create a basic layout of the jobs/show page

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,16 +1,17 @@
 class JobsController < ApplicationController
 
- def show
-  @locations = Location.near(params[:search], 25)
-   @hash = Gmaps4rails.build_markers(@locations) do |locations, marker|
-     marker.lat locations.latitude
-     marker.lng locations.longitude
-   end
+  def show
+    @location = Location.find_by(address: params[:search])
+    @locations = Location.near(params[:search], 5)
+      @hash = Gmaps4rails.build_markers(@locations) do |locations, marker|
+        marker.lat locations.latitude
+        marker.lng locations.longitude
+      end
 
- #   if @locations.empty?
- #     redirect_to "/"
- #   elsif @locations.nil?
- #     redirect_to "/"
- #   end
+   if @locations.empty?
+     redirect_to "/"
+   elsif @locations.nil?
+     redirect_to "/"
+   end
   end
 end
