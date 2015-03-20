0b7bcb2dd766bcc54b95c5619e24f0faf695efc0
Peterortiz82 <peterortiz82@gmail.com>
2015-03-19 23:36:47 -0400

    remove migration

--- db/migrate/20150316232427_remove_location_columns_from_jobs.rb
+++
@@ -1,7 +0,0 @@
-class RemoveLocationColumnsFromJobs < ActiveRecord::Migration
-  def change
-    remove_column :jobs, :latitude, :float
-    remove_column :jobs, :longitude, :float
-    remove_column :jobs, :address, :string
-  end
-end
