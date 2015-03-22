3e8158d8371e13fc00e248b87898985964fdfb06
jspencerhopkins <jspencerhopkins@gmail.com>
2015-03-22 16:18:38 -0400

    Paginate working - but still need to fix some kinks

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,16 +2,18 @@ class JobsController < ApplicationController
 
   def show
     @location = Location.find_by(address: params[:search].downcase)
-    @locations = Location.near(params[:search], 5)
-      @hash = Gmaps4rails.build_markers(@locations) do |locations, marker|
-        marker.lat locations.latitude
-        marker.lng locations.longitude
+    # @locations = Location.near(params[:search], 5)
+      @hash = Gmaps4rails.build_markers(@location) do |location, marker|
+        marker.lat location.latitude
+        marker.lng location.longitude
       end
 
-   if @locations.empty?
-     redirect_to "/"
-   elsif @locations.nil?
-     redirect_to "/"
-   end
+    @jobs = @location.jobs.paginate(:page => params[:page], :per_page => 6)
+
+   # if @location.empty?
+   #   redirect_to "/"
+   # elsif @location.nil?
+   #   redirect_to "/"
+   # end
   end
 end
