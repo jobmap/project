8cc4bbd79127a4656bc102bd4e63a2b739056650
Peterortiz82 <peterortiz82@gmail.com>
2015-03-23 11:20:06 -0400

    generate favorites model and associations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150323142306) do
+ActiveRecord::Schema.define(version: 20150323151050) do
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "al_job_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "jobs", force: :cascade do |t|
     t.string   "al_url"
