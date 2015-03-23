ac4e970a8595392652ccb591701b7af37f407f0a
Peterortiz82 <peterortiz82@gmail.com>
2015-03-23 11:28:59 -0400

    set associations for users/jobs/favorites

--- app/models/job.rb
+++ app/models/job.rb
@@ -4,6 +4,8 @@ class Job < ActiveRecord::Base
 
   belongs_to :location, foreign_key: 'al_loc_id'
   belongs_to :startup, foreign_key: 'al_start_id'
+  has_many   :favorites, :dependent => :destroy
+  has_many   :favorited, :through => :favorites, :source => :user
 
   validates :al_start_id, uniqueness: true
 
