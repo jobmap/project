d110cf1c4a12bbe17d938def8d3b0445aeba60ea
Peterortiz82 <peterortiz82@gmail.com>
2015-03-25 13:08:09 -0400

    allow users to favorite startups/display favorited startups on show page & delete favorited startups

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,9 +1,23 @@
 class FavoritesController < ApplicationController
 
+  def show
+    @favorites = current_user.jobs
+  end
+
   def create
+     @job = Job.find(params[:id])
+    if current_user.jobs<<(@job)
+      redirect_to jobs_url(search: params[:search]), notice: 'Job has been favorited'
+    else
+      redirect_to jobs_url(search: params[:search]), notice: 'Something went wrong...*sad panda*'
+    end
+  end
+
+  def destroy
+    @favorites = current_user.jobs
     @job = Job.find(params[:id])
-    @fav = current_user.jobs<<(@job)
-    redirect_to jobs_url(search: params[:search])
+    @favorites.destroy(@job)
+    redirect_to favorites_url, notice: 'Project is no longer in favorites'
   end
 
 
